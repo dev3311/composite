2c7e1c31dc5804bf74a7387a21b94a009303904d
dev3311 <159578326+dev3311@users.noreply.github.com>
2024-04-03 11:40:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from joblib import dump, load
+import seaborn as sns
+
+
+app = Flask(__name__)
+
+model = load('rock_poly_model.joblib') 
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+   
+    try:
+
+        in_value1 = float(request.form['input_value1'])
+       
+        
+        input_feature = [[in_value1]]
+
+      
+        prediction = model.predict(input_feature)
+        
+        output=round(prediction[0],2)
+        
+    
+
+        return render_template('index.html', prediction_text = 'Rock density is {}'.format(output))
+     
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    
+
+
+    
+if __name__ == '__main__':
+    app.run()    
